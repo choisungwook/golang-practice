b7996f94151d273e972f177afa890bbdfa991491
choisungwook <kgg1959@naver.com>
2021-12-20 20:29:23 +0900

    파일쓰기 #1

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
-	hand, remain := deal(cards, 5)
-	hand.print()
-	remain.print()
+	// fmt.Println(cards.toString())
+	// hand, remain := deal(cards, 5)
+	// hand.print()
+	// remain.print()
+	cards.saveToFile("result.txt")
 }

f0f0c943b9ca3f380d9678f0e8ffe6301e00121d
choisungwook <kgg1959@naver.com>
2021-12-22 21:23:58 +0900

    구조체 접근 #4

--- struct/main.go
+++ struct/main.go
@@ -11,4 +11,9 @@ func main() {
 	alex := person{firstName: "hello", lastName: "world"}
 	fmt.Println(alex)
 	fmt.Printf("%+v", alex)
+
+	var alpha person
+	alpha.firstName = "abc"
+	alpha.lastName = "def"
+	fmt.Printf("%+v", alpha)
 }
